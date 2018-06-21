e3af9176d182699edb252e3a5da0e298f7ec3b4c
Luxuride <39569690+Luxuride@users.noreply.github.com>
2018-06-21 19:48:24 +0200

    Edited name in main window Added connection to server Added close window

--- WPFClient/MainWindow.xaml.cs
+++ WPFClient/MainWindow.xaml.cs
@@ -29,13 +29,41 @@ namespace WPFClient
         public MainWindow()
         {
             InitializeComponent();
+            init("Luxuride");
             Connection();
+            Closing += MainWindow_Closing;
         }
 
-        private void LbChat_KeyDown(object sender, KeyEventArgs e)
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if (e.Key == Key.Enter) ;
+            Task shutdownTask = new Task(() =>
+            {
+                byte[] outStream = System.Text.Encoding.Unicode.GetBytes("\0CLIMSG\0exit");
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Close(1000);
+            });
+            shutdownTask.Start();
+            shutdownTask.Wait();
+            Environment.Exit(0);
+        }
 
+        private async void TxbInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                await Send();
+        }
+        private async void BtnSend_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            await Send();
+        }
+        private async Task Send()
+        {
+            if (TxbInput.Text.Length >= 0)
+            {
+                byte[] sendBite = System.Text.Encoding.Unicode.GetBytes(TxbInput.Text);
+                TxbInput.Text = null;
+                await serverStream.WriteAsync(sendBite, 0, sendBite.Length);
+            }
         }
 
         private void Connection()
@@ -69,6 +97,29 @@ namespace WPFClient
                         Console.WriteLine(ex);
                     }
             }).Start();
+        }
+        public async void init(string username)
+        {
+            System.Console.WriteLine("INIT");
+            try
+            {
+#if DEBUG
+                clientSocket.Connect("127.0.0.1", 8888);
+#else
+            clientSocket.Connect("TheArcaneBrony.ddns.net", 8888);
+#endif
+                UserName.Text = $"Connected as {username}";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                UserName.Text = "Connection failed!";
+            }
+
+
+            serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.Unicode.GetBytes("/nick " + username);
+            await serverStream.WriteAsync(outStream, 0, outStream.Length);
         }
     }
 }
