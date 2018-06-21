a6aa4e572063e46bd3883d326d6de95fba69d256
TheArcaneBrony <myrainbowdash949@gmail.com>
2018-06-21 21:59:04 +0200

    commit

--- WPFClient/MainWindow.xaml.cs
+++ WPFClient/MainWindow.xaml.cs
@@ -14,9 +14,11 @@ using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Application = System.Windows.Forms.Application;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
 
@@ -29,34 +31,82 @@ namespace WPFClient
     {
         public System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
         NetworkStream serverStream;
+
         public MainWindow()
         {
             InitializeComponent();
-            //init("Luxuride");
+            init("Luxuride");
+            ConnectingWindow cw = new ConnectingWindow(this);
+            cw.Show();
             Connection();
             Closing += MainWindow_Closing;
+            CloseButton.MouseUp += (sender, args) => { shutdown(); };
         }
 
         private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+         shutdown();
+        }
+
+        public void shutdown()
+        {/*
+            var animation = new DoubleAnimation
+            {
+                To = 0,
+                Duration = TimeSpan.FromSeconds(2),
+                FillBehavior = FillBehavior.Stop,
+            };
+
+            animation.Completed += (s, a) => Opacity = 0;
+
+            Titlebar.BeginAnimation(OpacityProperty, animation);*/
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.To = 0;
+            //animation.From = 1;
+            animation.Duration = TimeSpan.FromMilliseconds(3000);
+            animation.EasingFunction = new QuadraticEase();
+
+            Storyboard sb = new Storyboard();
+            sb.Children.Add(animation);
+
+            Titlebar.Opacity = 1;
+            Titlebar.Visibility = Visibility.Visible;
+
+            Storyboard.SetTarget(sb, Titlebar);
+            Storyboard.SetTargetProperty(sb, new PropertyPath(System.Windows.Controls.Control.OpacityProperty));
+
+            sb.Begin();
+
+            sb.Completed += delegate (object sender, EventArgs e)
+            {
+                Titlebar.Visibility = Visibility.Collapsed;
+            };
             Task shutdownTask = new Task(() =>
             {
                 byte[] outStream = System.Text.Encoding.Unicode.GetBytes("\0CLIMSG\0exit");
                 serverStream.Write(outStream, 0, outStream.Length);
                 serverStream.Close(1000);
+               /* Dispatcher.Invoke(() => {
+                    for (float i = 1.0f; i > 0.0; i -= 0.00005f * 250/*0.00005f*///)
+                   /* {
+                        Opacity = i;
+                        InvalidateVisual();
+                        UpdateLayout();
+                        //Console.WriteLine(i + "");
+                        Thread.Sleep(1000 / 120);
+                    }
+                });*/
+                
+                Thread.Sleep(5000);
             });
             shutdownTask.Start();
-            AllowsTransparency = true;
-            for (float i = 1.0f; i > 0.0; i -= 0.00005f * 250/*0.00005f*/)
-            {
-                Opacity = i;
-                //Console.WriteLine(i + "");
-                Thread.Sleep(1000 / 120);
-            }
 
-            shutdownTask.Wait();
+            
+        
+            //Thread.Sleep(3000);
+            shutdownTask.Wait(5000);
             Application.Exit();
-            Close();
+            //Close();
             Environment.Exit(0);
         }
 
@@ -81,9 +131,7 @@ namespace WPFClient
 
         private void Connection()
         {
-            ConnectingWindow connectingWindow = new ConnectingWindow(this);
-            connectingWindow.Show();
-            connectingWindow.Topmost = true;
+            
             new Thread(() =>
             {
                 while (true)
@@ -144,28 +192,54 @@ namespace WPFClient
             Console.WriteLine("Set the window title to: " + title);
         }
 
-       // Microsoft.Win32.UserPreferenceCategory
+        // Microsoft.Win32.UserPreferenceCategory
 
 
 
         //WIN API CODE
-        private const int WM_DWMCOLORIZATIONCOLORCHANGED = 0x320;
+
+
+       // Microsoft.Win32.SystemEvents.UserPreferenceChanged += SystemEvents_UserPreferenceChanged;
+
+        private void SystemEvents_UserPreferenceChanged(object sender, Microsoft.Win32.UserPreferenceChangedEventArgs e)
+        {
+            if (e.Category == Microsoft.Win32.UserPreferenceCategory.General)
+            {
+                // your code here, compare saved theme color with current one
+            }
+        }
+
         private const int WM_DWMCOMPOSITIONCHANGED = 0x31E;
         private const int WM_THEMECHANGED = 0x031A;
+        private IntPtr hwnd;
+        private HwndSource hsource;
 
-        protected  void WndProc(ref Message m)
+        private void Window_SourceInitialized(object sender, EventArgs e)
         {
-            switch (m.Msg)
+            if ((hwnd = new WindowInteropHelper(this).Handle) == IntPtr.Zero)
             {
-                case WM_DWMCOLORIZATIONCOLORCHANGED:
-                case WM_DWMCOMPOSITIONCHANGED:
-                case WM_THEMECHANGED:
-                    // you code here
-                    break;
-                default:
-                    break;
+                throw new InvalidOperationException("Could not get window handle.");
             }
-            //base.WndProc(ref m);
+
+            hsource = HwndSource.FromHwnd(hwnd);
+            hsource.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_DWMCOMPOSITIONCHANGED: // Define this as 0x31A
+                case WM_THEMECHANGED:          // Define this as 0x31E
+
+                    // Respond to DWM being enabled/disabled or system theme being changed
+
+
+                    return IntPtr.Zero;
+
+                default:
+                    return IntPtr.Zero;
+            }
         }
     }
     internal static class NativeMethods
